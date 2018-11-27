4b10354c53e99894aca97ac0b247aa393a382964
Vlad Ostrovsky <vladostr@us.ibm.com>
2018-11-27 13:27:57 -0500

    initial version

---
+++ TestJavaPrg/src/test/java/test_package/vladwebtest_01.java
@@ -0,0 +1,29 @@
+package test_package;
+
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+import org.testng.annotations.BeforeTest;	
+import org.testng.annotations.AfterTest;	
+
+public class vladwebtest_01 {
+	private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		System.setProperty("webdriver.gecko.driver","/opt/selenium/bin/geckodriver"); 
+		driver.get("http://rhel73test:6080/vladwebtest-0.0.1-SNAPSHOT/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Insert")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+		System.setProperty("webdriver.gecko.driver","/opt/selenium/bin/geckodriver"); 
+	    driver = new FirefoxDriver();  
+	}		
+	@AfterTest
+	public void afterTest() {
+		System.setProperty("webdriver.gecko.driver","/opt/selenium/bin/geckodriver"); 
+		driver.quit();			
+	}		
+}
